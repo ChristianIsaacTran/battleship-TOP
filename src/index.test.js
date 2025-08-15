9d42c734a09cb610a329737a329cc7a102ba29d7
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-08-14 20:47:35 -0500

    Added jest and test script in npm scripts
    I had to npm install @jest/globals to import
    jest explicitly since I am using type: modules in
    my package.json file. Again, some weirdness with
    ES6 modules will not allow me to run jest or mocks
    with jest normally, I would have to import them
    explicitly. I also imported @types/jest to
    let vscode recognize jest methods/functions to
    prevent incorrect auto-correcting. Also added a
    --watchAll flag to the npm test script to run
    tests whenever the files changed.

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,2 @@
 /* eslint-disable no-undef */
-import { add } from "./index.js";
-import { describe, test, jest, expect} from "@jest/globals";
-
-describe("this is a test that jest is working properly", () => {
-    test("test if add returns 2 with inputs 1 and 1 for parameters a and b", () => {
-        // mocked value
-        const mockedFunction = jest.fn();
-        mockedFunction.mockReturnValueOnce(2);
-        expect(mockedFunction()).toBe(2);
-    });
-});
+import { describe, test, jest, expect } from "@jest/globals";
