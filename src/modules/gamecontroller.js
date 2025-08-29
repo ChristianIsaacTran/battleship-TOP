47c6e670c086063dfa8e1bfe1d70723cc8e31e75
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-08-28 19:47:25 -0500

    feat: made a playRound() function in gamecontroller
    playRound() plays an entire round of battleship
    depending on the currentTurn. If its player 1's turn, then
    it will take a given attack coordinate, translate it to
    an array, then send the attack to player 2's board where player 2
    board will receive the attack, then it will update the attack history
    on player 2's board, then check for win condition. If it is not met,
    then swap turns. Same idea with player 2's turn. If the player 2
    is a computer, then it will get the randomly generated
    attack coordinate from the sendAttack() player method, then
    send it to player 1's board, then check for win condition and swap
    turns if the win condition is not met. I still need to test
    if the win condition activates properly when all ships are sunk.

--- src/modules/gamecontroller.js
+++ src/modules/gamecontroller.js
@@ -3,6 +3,7 @@ import player from "./player.js";
 export default function gamecontroller() {
     let player1;
     let player2;
+    let currentTurn;
 
     // makes player 1 with the player1Name parameter, and place ships. Player 1 cannot be a computer
     function makePlayer1(
@@ -34,6 +35,9 @@ export default function gamecontroller() {
             destroyerPos,
             destroyerFace,
         );
+
+        // start the game with player 1 as the first turn
+        currentTurn = player1;
     }
 
     // makes player 2 with the player2Name parameter, or if the player 2 is a computer, make a computer player. Player 2 can be a real person or a comuter.
@@ -84,5 +88,100 @@ export default function gamecontroller() {
         return player2;
     }
 
-    return { makePlayer1, getPlayer1, makePlayer2, getPlayer2 };
+    // utility function. returns currentTurn variable
+    function getCurrentTurn() {
+        return currentTurn;
+    }
+
+    /*
+     playRound() function. plays a full round, getting input from the current turn's player and making an attack on player 2
+     given attack coordinates, send the attack to the opposing player depending on whoever the current turn is, then check for a 
+     win condition on the opposing player. If no win, then swap turns. Also check if player 2 is a computer or not
+    */
+    function playRound(attackCoorY, attackCoorX) {
+        // current turn player1 =  Attack player 2 with given coordinates then check if all player 2's ships have been sunk. if not, swap to player 2
+        if (currentTurn === player1) {
+            // convert coordinates into attack array coordinates
+            const player1Attack = player1.sendAttack(attackCoorY, attackCoorX);
+
+            // send player 1 attack to player 2 board
+            const player2GameBoard = player2.getGameBoard();
+            player2GameBoard.receiveAttack(player1Attack);
+
+            // check if player 2 lost the game after attack
+            if (player2.checkPlayerLostGame()) {
+                console.log(
+                    "All ships from player 2 have been sunk. Player 1 wins!",
+                );
+                return;
+            }
+
+            // if the win condition is not true, then swap turns
+            swapCurrentTurn();
+        }
+        // if the currentTurn is player 2 and they ARE a computer player, then randomly generate an attack and send to player 1, check for win, then swap turns
+        else if (
+            currentTurn === player2 &&
+            player2.getComputerStatus() === true
+        ) {
+            // get random attack from player 2's send attack function
+            const player2Attack = player2.sendAttack();
+
+            // send player 2 Computer attack to player 1 board
+            const player1GameBoard = player1.getGameBoard();
+            player1GameBoard.receiveAttack(player2Attack);
+
+            // check win condition. If player 1 ships have been sunk after attack, declare winner
+            if (player1.checkPlayerLostGame()) {
+                console.log(
+                    "All ships from player 1 have been sunk. Player 2 (COMPUTER) wins!",
+                );
+                return;
+            }
+
+            swapCurrentTurn();
+        }
+        // if the currentTurn is player 2 and they are a REAL player, then send coordinates to player 1, check for win, then swap turns if win is not met
+        else if (
+            currentTurn === player2 &&
+            player2.getComputerStatus() === false
+        ) {
+            // convert coordinates into attack array coordinates
+            const player2Attack = player2.sendAttack(attackCoorY, attackCoorX);
+
+            // send player 2 attack to player 1 board
+            const player1GameBoard = player1.getGameBoard();
+            player1GameBoard.receiveAttack(player2Attack);
+
+            // check if player 1 lost the game after attack
+            if (player1.checkPlayerLostGame()) {
+                console.log(
+                    "All ships from player 1 have been sunk. Player 2 wins!",
+                );
+                return;
+            }
+
+            // if the win condition is not true, then swap turns
+            swapCurrentTurn();
+        }
+    }
+
+    // utility function, used to manipulate the currentTurn value for testing
+    function swapCurrentTurn() {
+        if (currentTurn === player1) {
+            currentTurn = player2;
+        } else {
+            currentTurn = player1;
+        }
+    }
+
+    return {
+        makePlayer1,
+        getPlayer1,
+        makePlayer2,
+        getPlayer2,
+        getCurrentTurn,
+        playRound,
+        swapCurrentTurn,
+    };
 }
--- src/modules/gamecontroller.test.js
+++ src/modules/gamecontroller.test.js
@@ -101,4 +101,132 @@ describe("Game controller tests", () => {
         expect(boardSpacesCounter).toBe(17);
     });
 
+    test("Make two players, then proceed to play a full round. Player 1 should make an attack on player 2", () => {
+        
+        // players are going to have identical boards with same ship placements
+        testGameController.makePlayer1(
+            "player 1",
+            ["B", "3"],
+            "down",
+            ["D", "2"],
+            "down",
+            ["C", "6"],
+            "right",
+            ["G", "9"],
+            "down",
+            ["I", "5"],
+            "right",
+        );
+
+        testGameController.makePlayer2(
+            "player 2",
+            false,
+            ["B", "3"],
+            "down",
+            ["D", "2"],
+            "down",
+            ["C", "6"],
+            "right",
+            ["G", "9"],
+            "down",
+            ["I", "5"],
+            "right",
+        );
+
+        // play 1 round and attack player 2's space at coordinate ["B","3"]
+        testGameController.playRound("B","3");
+
+        const player2 = testGameController.getPlayer2();
+
+        const player2GameBoard = player2.getGameBoard();
+
+        const player2AttackHistory = player2GameBoard.getAttackHistory();
+
+        expect(player2AttackHistory.has("3,2")).toBe(true);
+    });
+
+    test("make two players, then play a full round with player 2 being the computer. Have the computer send a randomized attack to player 1", () => {
+        // player 1 is real while player 2 is a computer player
+        testGameController.makePlayer1(
+            "player 1",
+            ["B", "3"],
+            "down",
+            ["D", "2"],
+            "down",
+            ["C", "6"],
+            "right",
+            ["G", "9"],
+            "down",
+            ["I", "5"],
+            "right",
+        );
+
+        testGameController.makePlayer2("", true);
+
+        // set the current turn to player 2 to test random computer attack
+        testGameController.swapCurrentTurn();
+
+        testGameController.playRound();
+
+        const player1 = testGameController.getPlayer1();
+        const player1GameBoard = player1.getGameBoard();
+        const player1AttackHistory = player1GameBoard.getAttackHistory();
+
+        let attackCounter = 0;
+
+        player1AttackHistory.forEach(() => {
+            attackCounter += 1;
+        });
+
+        // expect at least that one attack we send from that one round in the attack history of player 1
+        expect(attackCounter).toBe(1);
+
+    });
+
+    test("make two players, both real players, then have player 2 send an attack on player 1", () => {
+         // players are going to have identical boards with same ship placements
+        testGameController.makePlayer1(
+            "player 1",
+            ["B", "3"],
+            "down",
+            ["D", "2"],
+            "down",
+            ["C", "6"],
+            "right",
+            ["G", "9"],
+            "down",
+            ["I", "5"],
+            "right",
+        );
+
+        testGameController.makePlayer2(
+            "player 2",
+            false,
+            ["B", "3"],
+            "down",
+            ["D", "2"],
+            "down",
+            ["C", "6"],
+            "right",
+            ["G", "9"],
+            "down",
+            ["I", "5"],
+            "right",
+        );
+
+        // swap current turn to be player 2 turn
+        testGameController.swapCurrentTurn();
+
+        // player 2 is going to send an attack on player 1's carrier at ["B","3"]
+        testGameController.playRound("B","3");
+        
+        // check player 1's gameboard for an attack on position B,3
+        const player1 = testGameController.getPlayer1();
+        const player1GameBoard = player1.getGameBoard();
+        const player1AttackHistory = player1GameBoard.getAttackHistory();
+
+        console.log(player1AttackHistory);
+
+        expect(player1AttackHistory.has("3,2")).toBe(true);
+    });
 });
