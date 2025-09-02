7250e3c7477dd0c2e16f4c7ac4c1bf7a2ddbffed
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-09-01 19:49:56 -0500

    working: interfacecontroller() controlls the UI to interact with the game

---
+++ src/modules/interfacecontroller.js
@@ -0,0 +1,11 @@
+import gamecontroller from "./gamecontroller.js";
+
+export default function interfacecontroller() {
+
+    // renders the current boards for both players
+    function renderCurrentBoard() {
+        
+    }
+
+    return { renderCurrentBoard };
+}
