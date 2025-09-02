91ffbca8f3332e2663f10608450c9129fe1399f8
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-09-01 23:55:09 -0500

    feat: renderGamemodeSelection() shows game mode selection modal
    when the webpage starts up, it immediately shows the game
    mode selection screen through javascript.

--- src/modules/interfacecontroller.js
+++ src/modules/interfacecontroller.js
@@ -2,10 +2,41 @@ import gamecontroller from "./gamecontroller.js";
 
 export default function interfacecontroller() {
 
-    // renders the current boards for both players
-    function renderCurrentBoard() {
-        
+    // renders gamemode selection when upon webpage load
+    function renderGamemodeSelection() {
+        // create HTML elements
+        const body = document.querySelector("body");
+        const dialog = document.createElement("dialog");
+        const para = document.createElement("p");
+        const buttDiv = document.createElement("div");
+        const pvpButton = document.createElement("button");
+        const pvcButton = document.createElement("button");
+
+        // add attributes
+        dialog.setAttribute("class","gamemode-selection");
+        para.textContent = "Choose Gamemode";
+        buttDiv.setAttribute("class","gamemode-button-container");
+        pvpButton.setAttribute("class","player-vs-player-button");
+        pvpButton.textContent = "Player vs Player";
+        pvcButton.setAttribute("class","player-vs-computer");
+        pvcButton.textContent = "Player vs Computer";
+
+        // appendChild
+        buttDiv.appendChild(pvpButton);
+        buttDiv.appendChild(pvcButton);
+        dialog.appendChild(para);
+        dialog.appendChild(buttDiv);
+        body.appendChild(dialog);
+
+        window.addEventListener("load", () => {
+            dialog.showModal();
+        });
+
+
     }
 
-    return { renderCurrentBoard };
+    // renders the current boards for both players
+    function renderCurrentBoard() {}
+
+    return { renderCurrentBoard, renderGamemodeSelection };
 }
