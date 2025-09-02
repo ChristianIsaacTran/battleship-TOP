ebdfc01cafe735941064e0f81c68930be4f819f4
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-09-01 23:56:47 -0500

    working: made a template html dialog for renderGamemodeSelection()
    also changed the package.json to allow
    webpack to bundle and start a dev webpage by removing
    the type:module from package.json config file.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import interfacecontroller from "./modules/interfacecontroller.js";
+
+const userInterface = interfacecontroller();
+
+// shows game mode selection on window load
+userInterface.renderGamemodeSelection();
