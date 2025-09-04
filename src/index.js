45917b4e1b3d659e5e2f0ba5a33ab46e6eb8b072
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-09-03 23:04:33 -0500

    working: interactions between modals
    So the sequence of events I coded are:
    1. game mode selection
    if pvp:
        - go to player 1 option and create player 1
        - go to player 2 option and create player 2
        - start game
    if pvc:
        - go to player 1 option and create player 1,
        then also create a computer player 2
        - start game
    
    I was struggling with the event handlers for
    each case, since in both game modes, player 1
    options are displayed, so I needed to
    add a playerVsComputer boolean into renderPlayer1Options()
    to ensure that depending on which game mode is selected,
    the player 1 options will transition into the correct order
    as listed above. At the end of the day, it transitions
    and creates the player entries properly. I need to
    add form validation later, but for now the sequence works.
    I also added the "name" attributes to all the
    player inputs to give the values names when parsing them for
    form data.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import interfacecontroller from "./modules/interfacecontroller.js";
 
 const userInterface = interfacecontroller();
 
-// render player option modals 
-userInterface.renderPlayer1Options();
-userInterface.renderPlayer2Options();
-
 // shows game mode selection on window load
 userInterface.renderGamemodeSelection();
--- src/modules/interfacecontroller.js
+++ src/modules/interfacecontroller.js
@@ -51,18 +51,34 @@ export default function interfacecontroller() {
             // close the gamemode modal first
             dialog.close();
 
-            // get player 1 option modal and show it, then show player 2 modal after submitting player 1 options
+            // render player 1 and player 2 options menu
+            renderPlayer1Options();
+            renderPlayer2Options();
+
+            // get player 1 option modal and show it
             const player1Options = document.querySelector(
                 ".pvp-player1-options",
             );
             player1Options.showModal();
         });
 
-        pvcButton.addEventListener("click", () => {});
+        pvcButton.addEventListener("click", () => {
+            // close gamemode modal first
+            dialog.close();
+
+            // render player 1 options menu only, pass true to signal player vs computer
+            renderPlayer1Options(true);
+
+            // get player 1 option modal, then create a computer player 2
+            const player1Options = document.querySelector(
+                ".pvp-player1-options",
+            );
+            player1Options.showModal();
+        });
     }
 
     // renders the player 1 ship placement menu
-    function renderPlayer1Options() {
+    function renderPlayer1Options(playerVsComputer = false) {
         // generate html items
         const dialog = document.createElement("dialog");
         dialog.setAttribute("class", "pvp-player1-options");
@@ -81,7 +97,9 @@ export default function interfacecontroller() {
         p1NameInput.setAttribute("type", "text");
         p1NameInput.setAttribute("id", "p1-name");
         p1NameInput.setAttribute("maxlength", "20");
+        p1NameInput.setAttribute("name", "player1Name");
         p1NameInput.setAttribute("placeholder", "player 1 name goes here...");
+        const select = document.createElement("select");
 
         // construct form
         form.appendChild(optionHeader);
@@ -104,22 +122,31 @@ export default function interfacecontroller() {
             if (i === 0) {
                 shipLabel.textContent = "Carrier (5 spaces)";
                 shipInput.setAttribute("id", "carrier-p1");
+                shipInput.setAttribute("name", "carrier-p1-input");
+                select.setAttribute("name", "carrier-p1-dir");
             } else if (i === 1) {
                 shipLabel.textContent = "Battleship (4 spaces)";
                 shipInput.setAttribute("id", "battleship-p1");
+                shipInput.setAttribute("name", "battleship-p1-input");
+                select.setAttribute("name", "battleship-p1-dir");
             } else if (i === 2) {
                 shipLabel.textContent = "Cruiser (3 spaces)";
                 shipInput.setAttribute("id", "cruiser-p1");
+                shipInput.setAttribute("name", "cruiser-p1-input");
+                select.setAttribute("name", "cruiser-p1-dir");
             } else if (i === 3) {
                 shipLabel.textContent = "Submarine (3 spaces)";
                 shipInput.setAttribute("id", "submarine-p1");
+                shipInput.setAttribute("name", "submarine-p1-input");
+                select.setAttribute("name", "submarine-p1-dir");
             } else if (i === 4) {
                 shipLabel.textContent = "Destroyer (2 spaces)";
                 shipInput.setAttribute("id", "destroyer-p1");
+                shipInput.setAttribute("name", "destroyer-p1-input");
+                select.setAttribute("name", "destroyer-p1-dir");
             }
 
             // select dropdown menu for directions
-            const select = document.createElement("select");
             select.setAttribute("id", "directions");
             const left = document.createElement("option");
             left.setAttribute("value", "left");
@@ -159,6 +186,87 @@ export default function interfacecontroller() {
         dialog.appendChild(form);
         const body = document.querySelector("body");
         body.appendChild(dialog);
+
+        // form submit will transition into player 2 options if playerVsComputer = false, or just get player 1 inputs if playerVsComputer = true
+        if (playerVsComputer === false) {
+            form.addEventListener("submit", (e) => {
+                e.preventDefault();
+
+                // get player 1 form inputs and make player 1
+                const formData = new FormData(form);
+                const player1NameData = formData.get("player1Name");
+                const carrierPos = formData.get("carrier-p1-input");
+                const carrierFace = formData.get("carrier-p1-dir");
+                const battleshipPos = formData.get("battleship-p1-input");
+                const battleshipFace = formData.get("battleship-p1-dir");
+                const cruiserPos = formData.get("cruiser-p1-input");
+                const cruiserFace = formData.get("cruiser-p1-dir");
+                const submarinePos = formData.get("submarine-p1-input");
+                const submarineFace = formData.get("submarine-p1-dir");
+                const destroyerPos = formData.get("destroyer-p1-input");
+                const destroyerFace = formData.get("destroyer-p1-dir");
+
+                gameControl.makePlayer1(
+                    player1NameData,
+                    carrierPos,
+                    carrierFace,
+                    battleshipPos,
+                    battleshipFace,
+                    cruiserPos,
+                    cruiserFace,
+                    submarinePos,
+                    submarineFace,
+                    destroyerPos,
+                    destroyerFace,
+                );
+
+                // get and show player 2 modal after player 1
+                dialog.close();
+                const player2Options = document.querySelector(
+                    ".pvp-player2-options",
+                );
+                player2Options.showModal();
+            });
+        } else if (playerVsComputer === true) {
+            form.addEventListener("submit", (e) => {
+                e.preventDefault();
+
+                // get player 1 form inputs and make player 1
+                const formData = new FormData(form);
+                const player1NameData = formData.get("player1Name");
+                const carrierPos = formData.get("carrier-p1-input");
+                const carrierFace = formData.get("carrier-p1-dir");
+                const battleshipPos = formData.get("battleship-p1-input");
+                const battleshipFace = formData.get("battleship-p1-dir");
+                const cruiserPos = formData.get("cruiser-p1-input");
+                const cruiserFace = formData.get("cruiser-p1-dir");
+                const submarinePos = formData.get("submarine-p1-input");
+                const submarineFace = formData.get("submarine-p1-dir");
+                const destroyerPos = formData.get("destroyer-p1-input");
+                const destroyerFace = formData.get("destroyer-p1-dir");
+
+                gameControl.makePlayer1(
+                    player1NameData,
+                    carrierPos,
+                    carrierFace,
+                    battleshipPos,
+                    battleshipFace,
+                    cruiserPos,
+                    cruiserFace,
+                    submarinePos,
+                    submarineFace,
+                    destroyerPos,
+                    destroyerFace,
+                );
+
+                // since its player vs computer, after creating player 1, make a computer player 2
+                dialog.close();
+                gameControl.makePlayer2("", true);
+
+                console.log(gameControl.getPlayer1().getName());
+                console.log(gameControl.getPlayer2().getName());
+            });
+        }
     }
 
     // renders the player 2 ship placement menu
@@ -174,19 +282,20 @@ export default function interfacecontroller() {
         shipPlacementHeader.textContent = "Ship placements:";
         const nameGroup = document.createElement("div");
         nameGroup.setAttribute("class", "name-group");
-        const p1NameLabel = document.createElement("label");
-        p1NameLabel.setAttribute("for", "p2-name");
-        p1NameLabel.textContent = "Player 2 name:";
-        const p1NameInput = document.createElement("input");
-        p1NameInput.setAttribute("type", "text");
-        p1NameInput.setAttribute("id", "p2-name");
-        p1NameInput.setAttribute("maxlength", "20");
-        p1NameInput.setAttribute("placeholder", "player 2 name goes here...");
+        const p2NameLabel = document.createElement("label");
+        p2NameLabel.setAttribute("for", "p2-name");
+        p2NameLabel.textContent = "Player 2 name:";
+        const p2NameInput = document.createElement("input");
+        p2NameInput.setAttribute("type", "text");
+        p2NameInput.setAttribute("id", "p2-name");
+        p2NameInput.setAttribute("maxlength", "20");
+        p2NameInput.setAttribute("name", "player2Name");
+        p2NameInput.setAttribute("placeholder", "player 2 name goes here...");
 
         // construct form
         form.appendChild(optionHeader);
-        nameGroup.appendChild(p1NameLabel);
-        nameGroup.appendChild(p1NameInput);
+        nameGroup.appendChild(p2NameLabel);
+        nameGroup.appendChild(p2NameInput);
         form.appendChild(nameGroup);
         form.appendChild(shipPlacementHeader);
 
@@ -199,27 +308,37 @@ export default function interfacecontroller() {
             const shipInput = document.createElement("input");
             shipInput.setAttribute("type", "text");
             shipInput.setAttribute("placeholder", "placement position ex: B,1");
+            const select = document.createElement("select");
 
             // for each loop, change the appropriate ship label and id value for the ship inputs
             if (i === 0) {
                 shipLabel.textContent = "Carrier (5 spaces)";
                 shipInput.setAttribute("id", "carrier-p2");
+                shipInput.setAttribute("name", "carrier-p2-input");
+                select.setAttribute("name", "carrier-p2-dir");
             } else if (i === 1) {
                 shipLabel.textContent = "Battleship (4 spaces)";
                 shipInput.setAttribute("id", "battleship-p2");
+                shipInput.setAttribute("name", "battleship-p2-input");
+                select.setAttribute("name", "battleship-p2-dir");
             } else if (i === 2) {
                 shipLabel.textContent = "Cruiser (3 spaces)";
                 shipInput.setAttribute("id", "cruiser-p2");
+                shipInput.setAttribute("name", "cruiser-p2-input");
+                select.setAttribute("name", "cruiser-p2-dir");
             } else if (i === 3) {
                 shipLabel.textContent = "Submarine (3 spaces)";
                 shipInput.setAttribute("id", "submarine-p2");
+                shipInput.setAttribute("name", "submarine-p2-input");
+                select.setAttribute("name", "submarine-p2-dir");
             } else if (i === 4) {
                 shipLabel.textContent = "Destroyer (2 spaces)";
                 shipInput.setAttribute("id", "destroyer-p2");
+                shipInput.setAttribute("name", "destroyer-p2-input");
+                select.setAttribute("name", "destroyer-p2-dir");
             }
 
             // select dropdown menu for directions
-            const select = document.createElement("select");
             select.setAttribute("id", "directions");
             const left = document.createElement("option");
             left.setAttribute("value", "left");
@@ -259,6 +378,46 @@ export default function interfacecontroller() {
         dialog.appendChild(form);
         const body = document.querySelector("body");
         body.appendChild(dialog);
+
+        // form submit event for player 2
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            // get player 2 form inputs and make player 2
+            const formData = new FormData(form);
+            const player2NameData = formData.get("player2Name");
+            const carrierPos = formData.get("carrier-p2-input");
+            const carrierFace = formData.get("carrier-p2-dir");
+            const battleshipPos = formData.get("battleship-p2-input");
+            const battleshipFace = formData.get("battleship-p2-dir");
+            const cruiserPos = formData.get("cruiser-p2-input");
+            const cruiserFace = formData.get("cruiser-p2-dir");
+            const submarinePos = formData.get("submarine-p2-input");
+            const submarineFace = formData.get("submarine-p2-dir");
+            const destroyerPos = formData.get("destroyer-p2-input");
+            const destroyerFace = formData.get("destroyer-p2-dir");
+
+            // pass false for the isComputer boolean value in makePlayer2()
+            gameControl.makePlayer2(
+                player2NameData,
+                false,
+                carrierPos,
+                carrierFace,
+                battleshipPos,
+                battleshipFace,
+                cruiserPos,
+                cruiserFace,
+                submarinePos,
+                submarineFace,
+                destroyerPos,
+                destroyerFace,
+            );
+
+            console.log(gameControl.getPlayer1().getName());
+            console.log(gameControl.getPlayer2().getName());
+
+            dialog.close();
+        });
     }
 
     // renders the current boards for both players
@@ -267,7 +426,5 @@ export default function interfacecontroller() {
     return {
         renderCurrentBoard,
         renderGamemodeSelection,
-        renderPlayer1Options,
-        renderPlayer2Options,
     };
 }
